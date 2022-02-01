38875fd752fe453f3439c8d397fbb206092d92aa
odadki <93558662+odadki@users.noreply.github.com>
2022-01-31 17:34:15 -0800

    Added Dictionary.js and Dictionary.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Hello from App.js</header>
-      <btn className="btn btn-primary">Testing</btn>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">Coded by: Danielle Oda</footer>
     </div>
   );
 }
