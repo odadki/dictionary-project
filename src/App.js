268f809ce7eb0faf13a81d966ed6c914ac6e2076
odadki <93558662+odadki@users.noreply.github.com>
2022-01-31 16:44:42 -0800

    Added React and Bootstrap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello from App.js</header>
+      <btn className="btn btn-primary">Testing</btn>
+    </div>
+  );
+}
+
+export default App;
